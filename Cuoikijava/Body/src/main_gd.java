0a0299baa63a58ee471c40e458640d73bf85054e
Tran Quang <91446085+TranQuangBun@users.noreply.github.com>
2022-01-20 19:31:48 +0700

    Java Swing OOP - Manage Student

---
+++ Cuoikijava/Body/src/main_gd.java
@@ -0,0 +1,268 @@
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+
+public class main_gd extends JFrame{
+    JFrame frame = new JFrame("Quản Lý Thí Sinh Thi THPT - QUẢN LÝ");
+    JLabel idstudent = new JLabel("MÃ HỌC SINH");
+    JLabel name = new JLabel("HỌ TÊN");
+    JLabel genderlb = new JLabel("GIỚI TÍNH");
+    JLabel birthday = new JLabel("NGÀY SINH");
+    JLabel diemmontoan = new JLabel("ĐIỂM TOÁN");
+    JLabel diemmonvan = new JLabel("ĐIỂM VĂN");
+    JLabel diemmonanh = new JLabel("ĐIỂM ANH");
+
+    //bảng
+    JTable tbhouse = new JTable();
+    JScrollPane sp = new JScrollPane(tbhouse);
+    //db
+    PreparedStatement pst;
+    Connection conn = null;
+    ResultSet rs;
+    DefaultTableModel tm;
+
+    //textfield
+
+    JTextField idstudenttf = new JTextField(10);
+    JTextField nameTextField = new JTextField(100);
+    String[] gender = {"", "Nam", "Nữ"};
+    JComboBox cboGender = new JComboBox(gender);
+    JTextField birthdaytf = new JTextField(30);
+    JTextField toantf = new JTextField(10);
+    JTextField vantf = new JTextField(10);
+    JTextField anhtf = new JTextField(10);
+
+    //button
+
+    JButton themButton = new JButton("THÊM THÔNG TIN");
+    JButton resetButton = new JButton("HỦY BỎ");
+
+    JButton suaButton = new JButton("SỬA THÔNG TIN");
+    JButton xoaButton = new JButton("XÓA THÔNG TIN");
+    JButton findButton = new JButton("TÌM KIẾM THÔNG TIN THÍ SINH");
+    JButton refreshButton = new JButton("LÀM MỚI");
+     public main_gd(){
+//panel 1
+
+         frame.setLocation(100,20);
+         JPanel panel = new JPanel();
+         panel.add(idstudent);
+         panel.add(idstudenttf);
+         panel.add(name);
+         panel.add(nameTextField);
+
+         panel.add(genderlb);
+         panel.add(cboGender);
+
+         panel.add(birthday);
+         panel.add(birthdaytf);
+         panel.add(diemmontoan);
+         panel.add(toantf);
+         panel.add(diemmonvan);
+         panel.add(vantf);
+         panel.add(diemmonanh);
+         panel.add(anhtf);
+         panel.add(themButton);
+         panel.add(xoaButton);
+         panel.add(suaButton);
+         panel.add(resetButton);
+         panel.add(findButton);
+         panel.add(refreshButton);
+
+         panel.setBackground(Color.pink);
+         panel.setLayout(new GridLayout (10,2));
+         Border bd = BorderFactory.createLineBorder(Color.pink);
+//head title
+         TitledBorder tbdpnall= BorderFactory.createTitledBorder(bd, "QUẢN LÝ THÍ SINH THI VÀO LỚP 10 TRƯỜNG THPT ĐÀO DUY TỪ ");
+         tbdpnall.setTitleFont(new Font("Calibri", Font.BOLD, 15));
+         tbdpnall.setTitleColor(Color.black);
+         panel.setBorder(tbdpnall);
+
+//panel 2
+         JPanel pn = new JPanel();
+         pn.add(sp);
+         pn.setBackground(Color.pink);
+         Border bdtb = BorderFactory.createLineBorder(Color.pink);
+         TitledBorder tbtb= BorderFactory.createTitledBorder(bdtb, "THÔNG TIN THÍ SINH");
+         tbtb.setTitleFont(new Font("Calibri", Font.BOLD, 15));
+         tbtb.setTitleColor(Color.black);
+         pn.setBorder(tbtb);
+         Display();
+
+//set color
+         idstudent.setForeground(Color.black);
+         name.setForeground(Color.black);
+         birthday.setForeground(Color.black);
+         genderlb.setForeground(Color.black);
+         diemmontoan.setForeground(Color.black);
+         diemmonanh.setForeground(Color.black);
+         diemmonvan.setForeground(Color.black);
+
+         suaButton.setBackground(Color.BLUE);
+         suaButton.setForeground(Color.WHITE);
+         resetButton.setBackground(Color.BLUE);
+         resetButton.setForeground(Color.white);
+
+
+//Action
+
+         themButton.addActionListener(new ActionListener() {
+             @Override
+             public void actionPerformed(ActionEvent e) {
+                 ConnectDB condb = new ConnectDB();
+                 String ngaysinh = birthdaytf.getText();
+                 Date date = Date.valueOf(ngaysinh);
+                 String query = "INSERT INTO thisinh(mahocsinh,hoten,gioitinh,ngaysinh,toan,van,anh) VALUES (?,?,?,?,?,?,?)";
+                 conn = condb.connect_SQL();
+                 try{
+                     pst = conn.prepareStatement(query);
+                     pst.setString(1,idstudenttf.getText());
+                     pst.setString(2,nameTextField.getText());
+                     String s ;
+                     s = cboGender.getSelectedItem().toString();
+                     pst.setString(3,s);
+                     pst.setString(4,String.valueOf(date));
+                     pst.setString(5,toantf.getText());
+                     pst.setString(6,anhtf.getText());
+                     pst.setString(7,vantf.getText());
+                     pst.executeUpdate();
+                     JOptionPane.showMessageDialog(null, "Thêm thành công");
+                 }
+                 catch(Exception e1){
+                     e1.printStackTrace();
+                     JOptionPane.showMessageDialog(null," Thêm thất bại, vui lòng thử lại");
+                 }
+             }
+         });
+         resetButton.addActionListener(new ActionListener() {
+             @Override
+              public void actionPerformed(ActionEvent e) {
+                 if (e.getSource() == resetButton){
+                     idstudenttf.setText("");
+                     nameTextField.setText("");
+                     birthdaytf.setText("");
+                     toantf.setText("");
+                     vantf.setText("");
+                     anhtf.setText("");
+                     cboGender.setSelectedIndex(-1);
+                 }
+            }
+         });
+         suaButton.addActionListener(new ActionListener() {
+             @Override
+             public void actionPerformed(ActionEvent e) {
+             int ret = JOptionPane.showConfirmDialog(suaButton, "Bạn có muốn sửa không ?", "Xác nhận", JOptionPane.YES_NO_OPTION);
+                if(ret != JOptionPane.YES_OPTION){
+                    return;
+                }
+                 ConnectDB condb = new ConnectDB();
+                 String ngaysinh = birthdaytf.getText();
+                 Date date = Date.valueOf(ngaysinh);
+                 //  Date date = Date.valueOf(ngaysinh);
+                 String query = "UPDATE thisinh SET hoten=?,gioitinh =?, ngaysinh =? , toan =?, van =?, anh =? WHERE mahocsinh = ?";
+                 conn = condb.connect_SQL();
+                 try{ //:D
+                     pst = conn.prepareStatement(query);
+                     pst.setString(1,nameTextField.getText());
+                     String s ;
+                     s = cboGender.getSelectedItem().toString();
+                     pst.setString(2,s);
+                     pst.setString(3,ngaysinh);
+                     pst.setString(4,toantf.getText());
+                     pst.setString(5,vantf.getText());
+                     pst.setString(6,anhtf.getText());
+                     pst.setString(7,idstudenttf.getText());
+                     pst.executeUpdate();
+                     JOptionPane.showMessageDialog(null, "Cập nhật thành công");
+                 }
+                 catch(Exception e1){
+                     e1.printStackTrace();
+                     JOptionPane.showMessageDialog(null," Cập nhật thất bại, vui lòng thử lại");
+                 }
+             }
+
+         });
+         xoaButton.addActionListener(new ActionListener() {
+             @Override
+             public void actionPerformed(ActionEvent e) {
+                 int ret = JOptionPane.showConfirmDialog(xoaButton, "Bạn có muốn xóa không ?", "Xác nhận", JOptionPane.YES_NO_OPTION);
+                 if (ret != JOptionPane.YES_OPTION) {
+                     return;
+                 }
+              ConnectDB condb = new ConnectDB();
+              conn = condb.connect_SQL();
+                 String query = "DELETE FROM thisinh WHERE mahocsinh = ?";
+                 conn = condb.connect_SQL();
+                 try{
+                     pst = conn.prepareStatement(query);
+                     pst.setString(1,idstudenttf.getText());
+                     pst.executeUpdate();
+                     JOptionPane.showMessageDialog(null, "Xóa thành công");
+                 }
+                 catch(Exception e1){
+                     e1.printStackTrace();
+                     JOptionPane.showMessageDialog(null," Xóa thất bại, vui lòng thử lại");
+                 }
+             }
+         });
+         refreshButton.addActionListener(new ActionListener() {
+             @Override
+             public void actionPerformed(ActionEvent e) {
+                 ConnectDB cn = new ConnectDB();
+                 rs = cn.Danhsach("SELECT*FROM thisinh");
+                 String[] column = {"mahocsinh","hoten", "gioitinh", "ngaysinh", "toan", "van", "anh"};
+                 String gender = cboGender.getSelectedItem().toString();
+                 DefaultTableModel tm = new DefaultTableModel(column, 0);
+                 try {
+                     while (rs.next()) {
+                         Object data[] = {rs.getString("mahocsinh"), rs.getString("hoten"), rs.getString("gioitinh"), rs.getString("ngaysinh"), rs.getString("toan"), rs.getString("van"), rs.getString("anh")};
+                         tm.addRow(data);
+                     }
+                     tbhouse.setModel(tm);
+                 } catch (SQLException e1) {
+                     e1.printStackTrace();
+                 }
+             }
+         });
+         findButton.addActionListener(new ActionListener() {
+             @Override
+             public void actionPerformed(ActionEvent e) {
+                 new search_ts();
+             }
+         });
+//frame
+         frame.setVisible(true);
+         frame.pack();
+         frame.setSize(1200, 500);
+         frame.setLocationRelativeTo(null);
+         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+         Container cont = frame.getContentPane();
+         cont.add(panel, BorderLayout.CENTER);
+         cont.add(pn, BorderLayout.EAST);
+     }
+    public void Display(){
+
+        ConnectDB cn = new ConnectDB();
+        rs = cn.Danhsach("SELECT*FROM thisinh");
+        String[] column = {"mahocsinh","hoten", "gioitinh", "ngaysinh", "toan", "van", "anh"};
+        String gender = cboGender.getSelectedItem().toString();
+        DefaultTableModel tm = new DefaultTableModel(column, 0);
+        try {
+            while (rs.next()) {
+                Object data[] = {rs.getString("mahocsinh"), rs.getString("hoten"), rs.getString("gioitinh"), rs.getString("ngaysinh"), rs.getString("toan"), rs.getString("van"), rs.getString("anh")};
+                tm.addRow(data);
+            }
+            tbhouse.setModel(tm);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String [] agrs){
+         new main_gd();
+    }
+}
---
+++ Cuoikijava/Body/src/search_ts.java
@@ -0,0 +1,243 @@
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+import java.util.Vector;
+
+class search_ts extends JFrame {
+
+    JFrame f = new JFrame("TÌM KIẾM THÍ SINH");
+
+    JLabel idstudent = new JLabel("Mã học sinh");
+    JLabel name = new JLabel("Họ tên");
+    JLabel gender = new JLabel("Giới tính");
+    JLabel birthday = new JLabel("Ngày sinh");
+    JLabel toan = new JLabel("Điểm toán");
+    JLabel van = new JLabel("Điểm văn");
+    JLabel anh = new JLabel("Điểm anh");
+
+    JTextField idstudenttf = new JTextField(10);
+    JTextField nametf = new JTextField(10);
+    JTextField gendertf = new JTextField(10);
+    JTextField birthdaytf = new JTextField(10);
+    JTextField toantf = new JTextField(10);
+    JTextField vantf = new JTextField(10);
+    JTextField anhtf = new JTextField(10);
+    JTable tableSearch;
+    JButton searchButton = new JButton("TÌM KIẾM");
+    JButton returnButton = new JButton("QUAY LẠI");
+
+    PreparedStatement ps;
+    Connection conn;
+    ResultSet rs;
+
+    private void TaoFrame(){
+        f.setSize(1200,500);
+        f.add(TaoPanel());
+        f.setVisible(true);
+        f.setLocationRelativeTo(null);
+        f.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+    private JPanel TaoPanel() {
+        JPanel panel = new JPanel(new BorderLayout(7,7));
+        panel.add(input(), BorderLayout.NORTH);
+        panel.add(tbdate(), BorderLayout.CENTER);
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                panel.add(table(), BorderLayout.CENTER);
+                panel.revalidate();
+                reset();
+            }
+        });
+        panel.add(button(), BorderLayout.SOUTH);
+        return panel;
+    }
+    private JPanel input(){
+        JPanel panel = new JPanel(new GridLayout(2,7));
+        panel.add(idstudent);
+        panel.add(name);
+        panel.add(gender);
+        panel.add(birthday);
+        panel.add(toan);
+        panel.add(van);
+        panel.add(anh);
+
+        idstudent.setFont(new Font("Calibri", Font.BOLD, 15));
+        name.setFont(new Font("Calibri", Font.BOLD, 15));
+        gender.setFont(new Font("Calibri", Font.BOLD, 15));
+        birthday.setFont(new Font("Calibri", Font.BOLD, 15));
+        toan.setFont(new Font("Calibri", Font.BOLD, 15));
+        van.setFont(new Font("Calibri", Font.BOLD, 15));
+        anh.setFont(new Font("Calibri", Font.BOLD, 15));
+
+        panel.add(idstudenttf);
+        panel.add(nametf);
+        panel.add(gendertf);
+        panel.add(birthdaytf);
+        panel.add(toantf);
+        panel.add(vantf);
+        panel.add(anhtf);
+
+        return panel;
+    }
+    private JPanel button() {
+        JPanel panel = new JPanel(new GridLayout(1,1));
+        panel.add(searchButton);
+        searchButton.setFont(new Font("Calibri", Font.BOLD, 15));
+        searchButton.setBackground(Color.BLUE);
+        searchButton.setForeground(Color.WHITE);
+        //return button
+        panel.add(returnButton);
+        returnButton.setFont(new Font("Calibri", Font.BOLD, 15));
+        returnButton.setBackground(Color.BLUE);
+        returnButton.setForeground(Color.WHITE);
+        return  panel;
+    }
+    private JPanel table() {
+        JPanel panel = new JPanel(new GridLayout(1,1));
+        BangTimkiem();
+        JScrollPane sp = new JScrollPane(tableSearch);
+        panel.add(sp);
+        return  panel;
+    }
+    private JPanel tbdate(){
+        JPanel panel = new JPanel(new GridLayout(1,1));
+        BangDL();
+        JScrollPane sp = new JScrollPane(tableSearch);
+        panel.add(sp);
+        return  panel;
+    }
+    public void BangDL() {
+        ConnectDB connectdb = new ConnectDB();
+        String[] column = {"mahocsinh","hoten", "gioitinh", "ngaysinh", "toan", "van", "anh"};
+        DefaultTableModel tm1 = new DefaultTableModel(column, 0);
+
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+        try {
+            conn = connectdb.connect_SQL();
+            // tạo đối tượng thực thi câu lệnh SELECT
+            st = conn.createStatement();
+            // Thực thi
+            rs = st.executeQuery("SELECT * FROM thisinh");
+            Vector vtor = null;
+            tm1.setRowCount(0);
+            if (rs.isBeforeFirst() ==  false){
+                JOptionPane.showMessageDialog(this, "Không có thí sinh đó");
+                return;}
+            // Trong khi chưa hết dữ liệu
+            while (rs.next()){
+                vtor = new Vector();
+                vtor.add(rs.getString("mahocsinh"));
+                vtor.add(rs.getString("hoten"));
+                vtor.add(rs.getString("gioitinh"));
+                vtor.add(rs.getString("ngaysinh"));
+                vtor.add(rs.getString("toan"));
+                vtor.add(rs.getString("van"));
+                vtor.add(rs.getString("anh"));
+                // thêm dòng vào table model
+                tm1.addRow(vtor);
+            }
+            tableSearch = new JTable();
+            tableSearch.setModel(tm1); // thêm dữ liệu vào table
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+    public void BangTimkiem()  {
+        ConnectDB connectdb = new ConnectDB();
+        String[] column = {"mahocsinh","hoten", "gioitinh", "ngaysinh", "toan", "van", "anh"};
+        DefaultTableModel tm1 = new DefaultTableModel(column, 0);
+
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+        try {
+            conn = connectdb.connect_SQL();
+            // tạo đối tượng thực thi câu lệnh SELECT
+            st = conn.createStatement();
+            rs = st.executeQuery(sql_statement(idstudenttf.getText(),nametf.getText(),gendertf.getText(),birthdaytf.getText(), toantf.getText(), vantf.getText(), anhtf.getText()));
+            Vector vtor = null;
+            tm1.setRowCount(0);
+            if (rs.isBeforeFirst() ==  false){
+                JOptionPane.showMessageDialog(this, "Không có thí sinh đó");
+                return;
+            }
+            // Trong khi chưa hết dữ liệu
+            while (rs.next()){
+                vtor = new Vector();
+                vtor.add(rs.getString("mahocsinh"));
+                vtor.add(rs.getString("hoten"));
+                vtor.add(rs.getString("gioitinh"));
+                vtor.add(rs.getString("ngaysinh"));
+                vtor.add(rs.getString("toan"));
+                vtor.add(rs.getString("van"));
+                vtor.add(rs.getString("anh"));
+                tm1.addRow(vtor);
+            }
+            tableSearch = new JTable();
+            tableSearch.setModel(tm1);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+    public void reset() {
+        idstudenttf.setText("");
+        nametf.setText("");
+        gendertf.setText("");
+        birthdaytf.setText("");
+        toantf.setText("");
+        vantf.setText("");
+        anhtf.setText("");
+    }
+    public String sql_statement(String mahocsinh, String hoten, String gioitinh, String ngaysinh, String toan, String van, String anh ) {
+        // câu lệnh xem dữ liệu
+        String sql = "SELECT * FROM thisinh WHERE ";
+
+        if (mahocsinh.length()>0){
+            sql += (String)("mahocsinh like '%"+mahocsinh+"%'");
+        }
+        if (hoten.length()>0){
+            if (mahocsinh.length()>0) sql += " AND";
+            sql += (String)(" hoten like '%"+hoten+"%'");
+        }
+        if (gioitinh.length()>0){
+            if (mahocsinh.length()>0 || hoten.length()>0) sql += " AND";
+            sql += (String)(" Gioitinh like '%"+gioitinh+"%'");
+        }
+        if (ngaysinh.length()>0){
+            if (mahocsinh.length()>0 || hoten.length()>0 || gioitinh.length()>0) sql += " AND";
+            sql += (String)(" ngaysinh like '%"+ngaysinh+"%'");
+        }
+        if (toan.length()>0){
+            if (mahocsinh.length()>0 || hoten.length()>0 || gioitinh.length()>0 || ngaysinh.length()>0 ) sql += " AND";
+            sql += (String)(" toan like '%"+toan+"%'");
+        }
+        if (van.length()>0){
+            if (mahocsinh.length()>0 || hoten.length()>0 || gioitinh.length()>0 || ngaysinh.length()>0 || toan.length()>0) sql += " AND";
+            sql += (String)(" van like '%"+van+"%'");
+        }
+        if (anh.length()>0){
+            if (mahocsinh.length()>0 || hoten.length()>0 || gioitinh.length()>0 || ngaysinh.length()>0 || toan.length()>0 || van.length()>0) sql += " AND";
+            sql += (String)(" anh like '%"+anh+"%'");
+        }
+        return sql;
+    }
+    public search_ts(){
+        TaoFrame();
+        returnButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new main_gd();
+                f.setVisible(false);
+            }
+        });
+    }
+    public static void main(String[] args) {
+        new search_ts();
+    }
+}
